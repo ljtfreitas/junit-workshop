29b20b334bfba64a1d2a2346005bfa2a650d301c
Tiago de Freitas Lima <ljtfreitas@gmail.com>
2019-08-20 21:34:58 -0300

    Passo 1 (criacao de tarefa, teste quebrado)

--- src/main/java/br/com/soujava/Board.java
+++ src/main/java/br/com/soujava/Board.java
@@ -1,5 +1,17 @@
 package br.com.soujava;
 
+import java.time.Duration;
+import java.time.LocalTime;
+import java.util.Collections;
+import java.util.List;
+
 class Board {
 
+	Task addTask(String name, LocalTime start, Duration duration) {
+		return null;
+	}
+
+	List<Task> tasks() {
+		return Collections.emptyList();
+	}
 }
--- src/test/java/br/com/soujava/BoardTest.java
+++ src/test/java/br/com/soujava/BoardTest.java
@@ -1,14 +1,24 @@
 package br.com.soujava;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.Duration;
+import java.time.LocalTime;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
 class BoardTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void shouldAddNewTaskToBoard() {
+		Board board = new Board();
+
+        board.addTask("Whatever", LocalTime.parse("09:00"), Duration.ofHours(1));
+
+        List<Task> tasks = board.tasks();
+
+        assertEquals(1, tasks.size());
 	}
 
 }
