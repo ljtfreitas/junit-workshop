9ddfef415b7d9660633706db6058209ca2390666
Tiago de Freitas Lima <ljtfreitas@gmail.com>
2019-08-20 21:38:39 -0300

    Passo 2 (criacao de tarefa, teste passando)

--- src/main/java/br/com/soujava/Board.java
+++ src/main/java/br/com/soujava/Board.java
@@ -2,16 +2,22 @@ package br.com.soujava;
 
 import java.time.Duration;
 import java.time.LocalTime;
-import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 class Board {
 
-	Task addTask(String name, LocalTime start, Duration duration) {
-		return null;
+	private List<Task> tasks = new LinkedList<>();
+
+	Task addTask(String name, LocalTime startTime, Duration duration) {
+		Task newTask = new Task(name, startTime, duration);
+		
+		tasks.add(newTask);
+		
+		return newTask;
 	}
 
 	List<Task> tasks() {
-		return Collections.emptyList();
+		return tasks;
 	}
 }
--- src/main/java/br/com/soujava/Task.java
+++ src/main/java/br/com/soujava/Task.java
@@ -1,5 +1,18 @@
 package br.com.soujava;
 
+import java.time.Duration;
+import java.time.LocalTime;
+
 class Task {
+
+	final String name;
+	final LocalTime startTime;
+	final Duration duration;
+
+	Task(String name, LocalTime startTime, Duration duration) {
+		this.name = name;
+		this.startTime = startTime;
+		this.duration = duration;
+	}
 
 }
