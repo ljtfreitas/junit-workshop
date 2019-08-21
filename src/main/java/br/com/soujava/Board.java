d69f064d94961dccf957ad98777c74cb0056e7f3
Tiago de Freitas Lima <ljtfreitas@gmail.com>
2019-08-20 22:01:01 -0300

    Passo 7 (verificacao de horario muito cedo, passando)

--- src/main/java/br/com/soujava/Board.java
+++ src/main/java/br/com/soujava/Board.java
@@ -7,9 +7,15 @@ import java.util.List;
 
 class Board {
 
-	private List<Task> tasks = new LinkedList<>();
+	private static final LocalTime FIRST_HOUR_OF_DAY = LocalTime.parse("09:00");
+
+	private final List<Task> tasks = new LinkedList<>();
 
 	Task addTask(String name, LocalTime startTime, Duration duration) {
+		if (startTime.isBefore(FIRST_HOUR_OF_DAY)) {
+            throw new IllegalArgumentException(startTime + " it's too early...Go to bed");
+        }
+
 		Task newTask = new Task(name, startTime, duration);
 		
 		tasks.add(newTask);
