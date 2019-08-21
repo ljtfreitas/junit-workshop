279ec0f34e8ed50c8d9f89f1342c40d35d11dbd5
Tiago de Freitas Lima <ljtfreitas@gmail.com>
2019-08-20 21:29:55 -0300

    Criacao do projeto

---
+++ src/test/java/br/com/soujava/BoardTest.java
@@ -0,0 +1,14 @@
+package br.com.soujava;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+class BoardTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
