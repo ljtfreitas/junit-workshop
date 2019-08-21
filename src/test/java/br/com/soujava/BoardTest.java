23fa7b8637b01109135c46b02f76c0240f16fd37
Tiago de Freitas Lima <ljtfreitas@gmail.com>
2019-08-20 23:09:55 -0300

    Passo 22 (testes parametrizados, com horario e duracao parametrizada com MethodSource)

--- src/test/java/br/com/soujava/BoardTest.java
+++ src/test/java/br/com/soujava/BoardTest.java
@@ -3,16 +3,20 @@ package br.com.soujava;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 import java.time.Duration;
 import java.time.LocalTime;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 @DisplayName("Testes do quadro de tarefas")
 class BoardTest {
@@ -42,14 +46,20 @@ class BoardTest {
 
 	@DisplayName("Nao deve permitir criar tarefas em horarios de descanso")
     @ParameterizedTest(name = "Teste {index}: Nao deve permitir tarefas comecando as {0}, com {1}h de duracao")
-	@CsvFileSource(resources = "/horrible-times.csv")
-    void shouldThrowExceptionWhenTaskStartsInBreakTimes(String time, int hours) {
+	@MethodSource("horribleTimesToDoSomething")
+    void shouldThrowExceptionWhenTaskStartsInBreakTimes(String time, Duration duration) {
         LocalTime horribleTimeToDoSomething = LocalTime.parse(time);
-        Duration duration = Duration.ofHours(hours);
 
         HorribleTimeToDoSomethingException exception = assertThrows(HorribleTimeToDoSomethingException.class,
                 () -> board.addTask("Whatever", horribleTimeToDoSomething, duration));
         
         assertEquals(horribleTimeToDoSomething, exception.horribleTime);
     }
+	
+	static Collection<Arguments> horribleTimesToDoSomething() {
+		return Arrays.asList(arguments("08:30", Duration.ofHours(1)),
+							  arguments("19:30", Duration.ofHours(2)),
+							  arguments("18:30", Duration.ofHours(1)),
+							  arguments("16:00", Duration.ofHours(4)));
+	}
 }
