759717a6b0bda20a9a8a9df0353b3048e19568b1
Tiago de Freitas Lima <ljtfreitas@gmail.com>
2019-08-21 21:20:45 -0300

    Passo 31 (teste de execucao de tarefas, com anotacao Timeout)

--- src/test/java/br/com/soujava/TaskTest.java
+++ src/test/java/br/com/soujava/TaskTest.java
@@ -1,23 +1,25 @@
 package br.com.soujava;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
 
 import java.time.Duration;
 import java.time.LocalTime;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 
 @DisplayName("Testes de tarefas")
 class TaskTest {
 
 	@DisplayName("Deve executar a tarefa sem exceder o tempo limite")
 	@Test
+	@Timeout(unit = TimeUnit.SECONDS, value = 2)
 	void shouldExecuteTaskWithoutExceedTimeout() {
 		Task task = new Task("Do something", LocalTime.parse("09:00"), Duration.ofHours(1));
 
-		TaskResult result = assertTimeoutPreemptively(Duration.ofSeconds(2), () -> task.execute());
+		TaskResult result = task.execute();
 		
 		assertNotNull(result.description);
 	}
