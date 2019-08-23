deb0f590afa59732354962434b688ef88001a347
Tiago de Freitas Lima <ljtfreitas@gmail.com>
2019-08-22 21:43:30 -0300

    Passo 35 (tags e configuracoes no build)

---
+++ src/test/java/br/com/soujava/TagTest.java
@@ -0,0 +1,18 @@
+package br.com.soujava;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Exemplos de usos de tags")
+class TagTest {
+
+	@Test
+	@Tag("soujava")
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
